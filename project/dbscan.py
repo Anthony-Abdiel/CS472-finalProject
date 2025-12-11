4a576c8dafad2d30be8643cadca0bab7c77abae1
Anthony-Abdiel <aan266@nau.edu>
2025-12-11 16:55:28 -0700

    Finalized the website, and cleaned up the project directory, added files to this commit this time

--- project/dbscan.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-from sklearn.preprocessing import StandardScaler
-from sklearn.cluster import DBSCAN
-
-from sklearn.decomposition import PCA
-import matplotlib.pyplot as plt
--- project/kmedoids.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-from sklearn.preprocessing import StandardScaler
-from sklearn.cluster import 
-
-from sklearn.decomposition import PCA
-import matplotlib.pyplot as plt
